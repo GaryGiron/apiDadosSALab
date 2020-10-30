8885c9cd7a8c7b68598e6d36a7ef3a720daaf0be
GaryGiron <gsteph393@gmail.com>
2020-10-30 00:03:19 -0600

    Ultimas modificaciones dados

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ var express = require("express"),
     bodyParser  = require("body-parser"),
     methodOverride = require("method-override");
     mongoose = require('mongoose');
+//var jwt = require('jwt-simple');
+var env = require('node-env-file'); // .env file
+env(__dirname + '/.env');
+var jwt_decode = require('jwt-decode');
+
+var moment = require('moment');
+//var config = require('./config');;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,6 +23,21 @@ router.get('/', function(req, res) {
 
 router.get('/tirar/:number', function(req, res) {
     var vals=[];
+    if(!req.headers.authorization) {
+        return res
+            .status(403)
+            .send({message: "Tu petición no tiene cabecera de autorización"});
+    }
+    var tokenB = req.headers.authorization.split(" ")[1];
+    //var payload = jwt.decode(tokenB, 'fF7fCg6C');
+    var payload = jwt_decode(tokenB);
+    if(payload.exp <= moment().unix()) {
+        return res
+            .status(401)
+            .send({message: "El token ha expirado"});
+    }
+    //req.user = payload.sub;
+    //funcionalidad
     if(parseInt(req.param('number'),10)!=='NaN'){
         var i=0;
         var dados=parseInt(req.param('number'),10);
@@ -23,7 +45,10 @@ router.get('/tirar/:number', function(req, res) {
             vals.push(Math.floor(Math.random() * (6 - 1)) + 1);
         }
     }else{
-        vals.push(Math.floor(Math.random() * (6 - 1)) + 1);
+        //vals.push(Math.floor(Math.random() * (6 - 1)) + 1);
+        return res
+            .status(400)
+            .send({message: "Numero de dados no es valido"});
     }
     var result=`{
         "dados": [
