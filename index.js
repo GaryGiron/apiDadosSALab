71b7cbb8abb92e186153eeba7b6e9a417d811d9e
GaryGiron <gsteph393@gmail.com>
2020-11-07 01:33:32 -0600

    Validacion de JWT

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ var express = require("express"),
     bodyParser  = require("body-parser"),
     methodOverride = require("method-override");
     mongoose = require('mongoose');
-//var jwt = require('jwt-simple');
+const fs = require('fs');
+const public_key = fs.readFileSync('./key-public.pem');
+const jwt = require('jsonwebtoken');
 var env = require('node-env-file'); // .env file
 env(__dirname + '/.env');
 var jwt_decode = require('jwt-decode');
 
 var moment = require('moment');
-//var config = require('./config');;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
+
 
 var router = express.Router();
 
@@ -23,6 +25,7 @@ router.get('/', function(req, res) {
 
 router.get('/tirar/:number', function(req, res) {
     var vals=[];
+    //No trae autorizacion
     if(!req.headers.authorization) {
         return res
             .status(403)
@@ -30,33 +33,65 @@ router.get('/tirar/:number', function(req, res) {
     }
     var tokenB = req.headers.authorization.split(" ")[1];
     //var payload = jwt.decode(tokenB, 'fF7fCg6C');
-    var payload = jwt_decode(tokenB);
+    const bearerHeader =  req.headers.authorization;
+    //VALIDACION DE JWT
+    if(typeof bearerHeader !== 'undefined'){
+    const bearerToken = bearerHeader.split(" ")[1];
+    jwt.verify(bearerToken, public_key, { algorithms: ['RS256'] }, (err) => {
+        //se esta enviando un token ya vecido
+        if(err !=null){
+        if (err.name === 'TokenExpiredError') {
+            res.status(470);
+            res.send('Whoops, your token has expired!');
+        }
+        //se envia un token invalido
+        if (err.name === 'JsonWebTokenError') {
+            
+            res.status(471);
+            res.send('That JWT is malformed!');
+        }
+        }
+        //si jalo el token
+        if (err === null) {
+            if(parseInt(req.param('number'),10)!=='NaN'){
+                var i=0;
+                var dados=parseInt(req.param('number'),10);
+                for(i=0;i<dados;i++){
+                    vals.push(Math.floor(Math.random() * (6 - 1)) + 1);
+                }
+            }else{
+                //vals.push(Math.floor(Math.random() * (6 - 1)) + 1);
+                return res
+                    .status(400)
+                    .send({message: "Numero de dados no es valido"});
+            }
+            var result=`{
+                "dados": [
+                    `+vals+`
+                ]
+              }`;
+            console.log('Your JWT was successfully validated!');
+            result=JSON.parse(result);
+            res.status(201);
+            res.send(result);
+            
+        }
+        
+        // Both should be the same
+    
+    });
+    }else{
+    console.log("indefinido");
+    }
+    /*var payload = jwt_decode(tokenB);
     if(payload.exp <= moment().unix()) {
         return res
             .status(401)
             .send({message: "El token ha expirado"});
-    }
+    }*/
     //req.user = payload.sub;
     //funcionalidad
-    if(parseInt(req.param('number'),10)!=='NaN'){
-        var i=0;
-        var dados=parseInt(req.param('number'),10);
-        for(i=0;i<dados;i++){
-            vals.push(Math.floor(Math.random() * (6 - 1)) + 1);
-        }
-    }else{
-        //vals.push(Math.floor(Math.random() * (6 - 1)) + 1);
-        return res
-            .status(400)
-            .send({message: "Numero de dados no es valido"});
-    }
-    var result=`{
-        "dados": [
-            `+vals+`
-        ]
-      }`;
-    result=JSON.parse(result);
-    res.send(result);
+    
 });
 
 
