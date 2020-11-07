212c21d047144c7d19726cb56d5b306cd6bc7a8e
GaryGiron <gsteph393@gmail.com>
2020-11-07 11:41:05 +0000

    Se agrega el log de dados

--- index.js
+++ index.js
@@ -27,6 +27,12 @@ router.get('/tirar/:number', function(req, res) {
     var vals=[];
     //No trae autorizacion
     if(!req.headers.authorization) {
+        fs.appendFile('log.txt', 'No se tiene cabecera de autorizacion', function(err) {
+         // If an error occurred, show it and return
+         if(err) return console.error(err);
+         // Successfully wrote to the file!
+        });
+        console.log('La peticion no cuenta con cabecera de autorizacion');
         return res
             .status(403)
             .send({message: "Tu petición no tiene cabecera de autorización"});
@@ -41,18 +47,26 @@ router.get('/tirar/:number', function(req, res) {
         //se esta enviando un token ya vecido
         if(err !=null){
         if (err.name === 'TokenExpiredError') {
+             fs.appendFile('log.txt', 'El Token Validado '+bearerToken+' se encuentra expirado\n', function(err) {
+             // If an error occurred, show it and return
+             if(err) return console.error(err);
+             // Successfully wrote to the file!
+            });
             res.status(470);
-            res.send('Whoops, your token has expired!');
+            res.send('Whoops, Tu token ha expirado!!!');
         }
         //se envia un token invalido
         if (err.name === 'JsonWebTokenError') {
-            
+             fs.appendFile('log.txt', 'El Token Validado '+bearerToken+' muestra que el JWT esta incorrecto\n', function(err) {
+              // If an error occurred, show it and return
+              if(err) return console.error(err);
+              // Successfully wrote to the file!
+            });
+
             res.status(471);
-            res.send('That JWT is malformed!');
+            res.send('Este JWT esta incorrecto!');
         }
-        }
-        //si jalo el token
-        if (err === null) {
+        }else{
             if(parseInt(req.param('number'),10)!=='NaN'){
                 var i=0;
                 var dados=parseInt(req.param('number'),10);
@@ -61,6 +75,12 @@ router.get('/tirar/:number', function(req, res) {
                 }
             }else{
                 //vals.push(Math.floor(Math.random() * (6 - 1)) + 1);
+               fs.appendFile('log.txt', 'El numero de dados enviado, es incorrecto\n', function(err) {
+                  // If an error occurred, show it and return
+               if(err) return console.error(err);
+                 // Successfully wrote to the file!
+               });
+
                 return res
                     .status(400)
                     .send({message: "Numero de dados no es valido"});
@@ -70,7 +90,15 @@ router.get('/tirar/:number', function(req, res) {
                     `+vals+`
                 ]
               }`;
-            console.log('Your JWT was successfully validated!');
+            fs.appendFile('log.txt', 'El Token Validado '+bearerToken+' se genero exitosamente.\n Se tiene el resultado: '+result, function(err) {
+              // If an error occurred, show it and return
+              if(err) return console.error(err);
+              // Successfully wrote to the file!
+            });
+
+            console.log('Se tiene el resultado:');
+            console.log(result);
+            console.log('Tu JWT fue validado Exitosamente!');
             result=JSON.parse(result);
             res.status(201);
             res.send(result);
